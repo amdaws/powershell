4b180341fdf251d3145ac487a92d05cb433fd99d
Aaron Daws <dawsdev@outlook.com>
2021-10-25 11:00:22 +1000

    Update functions.psm1

---
+++ functions.psm1
@@ -0,0 +1,11 @@
+function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs=""
+    return [String]$characters[$random]
+}
+function Scramble-String([string]$inputString){     
+    $characterArray = $inputString.ToCharArray()   
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+    $outputString = -join $scrambledStringArray
+    return $outputString 
+}
