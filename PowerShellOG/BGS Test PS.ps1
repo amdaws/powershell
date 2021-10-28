2c0129756a0fa70ab31dc7041c127b482919b661
Aaron Daws <dawsdev@outlook.com>
2021-10-29 09:37:38 +1000

    PS-OGs

---
+++ PowerShellOG/BGS Test PS.ps1
@@ -0,0 +1,10 @@
+﻿
+$emlBody += "This is a test email"
+#====================================================================================
+$addressFrom = "useroutput@brisbanegrammar.com" 
+$addressTo = "adm.aaron@brisbanegrammar.com"
+#$addressTo = "edb1cf44.brisbanegrammarcom.onmicrosoft.com@apac.teams.ms" 
+$emlSubject = "Student passwords changed on "+(Get-Date) 
+$smtpServer = "relay.brisbanegrammar.com"
+#Send Email
+Send-MailMessage -SmtpServer $smtpServer -to $addressTo -from $addressFrom -Subject $emlSubject -body $emlBody -bodyasHTML
